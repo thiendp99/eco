db6d82bf5e1f46cd4dfafd4e6ac7424e144eed9d
thiendp99 <thiendp99@gmail.com>
2026-02-24 13:06:13 +0700

    fix: remove declare module shoppingCart/CartStore

--- packages/host/src/types/remotes.d.ts
+++ packages/host/src/types/remotes.d.ts
@@ -21,9 +21,3 @@ declare module 'shoppingCart/CartButton' {
   const CartButton: ComponentType;
   export default CartButton;
 }
-
-declare module 'shoppingCart/CartStore' {
-  import { StoreApi, UseBoundStore } from 'zustand';
-  import { CartStore } from '@ecommerce/shared';
-  export const useCartStore: UseBoundStore<StoreApi<CartStore>>;
-}
